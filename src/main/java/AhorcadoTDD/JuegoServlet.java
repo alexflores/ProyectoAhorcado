1d43f04cc07bbd2c218ff35f66df9047ff64e774
alexflores <axel_f_g@hotmail.com>
2013-11-20 18:19:20 -0400

    primer commit

---
+++ src/main/java/AhorcadoTDD/JuegoServlet.java
@@ -0,0 +1,18 @@
+package AhorcadoTDD;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JuegoServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String letrita = request.getParameter("letra");
+		response.getWriter().println(letrita);
+		
+	}
+}
---
+++ src/main/java/AhorcadoTDD/Principal.java
@@ -0,0 +1,10 @@
+package AhorcadoTDD;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
