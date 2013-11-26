116c94bda8016691c5d34aef6d7fe99520348159
alexflores <axel_f_g@hotmail.com>
2013-11-26 08:15:06 -0400

    Test juego devuelve incorrectogit status si letra no es de la palabra

--- src/test/java/AhorcadoTest/TestJuego.java
+++ src/test/java/AhorcadoTest/TestJuego.java
@@ -2,30 +2,46 @@ package AhorcadoTest;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import AhorcadoTDD.Juego;
 import AhorcadoTDD.Letra;
 
 public class TestJuego {
+	private Juego j;
+	
+	@Before
+	public void setup() {
+		j = new Juego();
+		j.iniciarJuego("sistemas");
+	}
 
 	@Test
 	public void devuelveTrueSiLaLetraJugadaPerteneceALaPalabra() {
-		Juego j = new Juego();
+
 		Letra l = new Letra('s');
-		j.iniciarJuego("sistemas");
+		//j.iniciarJuego("sistemas");
 		assertEquals(true,j.jugarLetra(l));
 	}
 	
 	@Test
 	public void devuelveMensajeDeIngresarDeLaAalaZEnCasoDeSerTamMayorAUno()
 	{
-		Juego j = new Juego();
+
 		String pal = "sistemas";
 		char arr[] = pal.toCharArray();
 		int tam = arr.length;
 		Letra l = new Letra(arr[0]);
 		assertEquals("Debe ingresar una letra de la A a la Z",j.juego(l, tam));
+	}
+	
+	@Test
+	public void devuelveIncorrectoSiLaLetraJugadaNoPerteneceALaPalabra()
+	{
+		Letra l = new Letra('k');
+		//j.iniciarJuego("sistemas");
+		assertEquals("Incorrecto!!!",j.juego(l, 1));
 	}
 
 }
