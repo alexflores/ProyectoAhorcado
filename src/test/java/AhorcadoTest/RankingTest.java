3eab5b7c3cef5741c44f8dd0bf40528aa2f2aafd
RudyEscalera <morbocuby@hotmail.com>
2013-11-27 11:18:58 -0300

    eliminando comentarios

--- src/test/java/AhorcadoTest/RankingTest.java
+++ src/test/java/AhorcadoTest/RankingTest.java
@@ -8,19 +8,6 @@ import AhorcadoTDD.Ranking;
 
 public class RankingTest {
 	Ranking nuevo;
-	/*@Test
-	public void convertirastring()  seguramente esta fallando algun extend
-	{
-		int entero=15;
-		String nombre="trancapecho";
-		assertEquals("trancapecho%15",nuevo.nuevostring(nombre, entero));
-	}*/
-	/*@Test  es tonto... no corre el test y solo esta regresando 50
-	public void puntaje()
-	{
-		int fallas=2;
-		assertEquals(50,nuevo.puntajetotal(fallas));
-	}*/
 	@Test
 	public void test() {
 		assertEquals(1,1);
