627bfa54a2f61e9808d6fd59550122d87aa55a14
treicko <leo_mejia_p@hotmail.com>
2013-11-27 22:03:37 -0400

    arreglando errores

--- src/main/java/AhorcadoTDD/Principal.java
+++ src/main/java/AhorcadoTDD/Principal.java
@@ -4,7 +4,11 @@ public class Principal {
 	
 	public static void main(String[] args)
 	{
-		
+		//dic.palabraDiccionario = "sistemas";
+		//dic.fraseDiccionario = "permite estudiar y comprender la realidad, con el propósito de implementar u optimizar sistemas complejos";
+		//dic.AniadirPalabra(dic);
+		//pal = dic.ObtenerPalabra();
+		//j.iniciarJuego(pal);
 	}
 	
  }
